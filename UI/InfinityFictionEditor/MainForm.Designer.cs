c5932cf619e4c80bf0d1a4cbd716ea71373c8a4d
Blind-Striker <deniz_irgin@yahoo.com>
2013-07-30 09:47:21 +0300

    Menu added

--- UI/InfinityFictionEditor/MainForm.Designer.cs
+++ UI/InfinityFictionEditor/MainForm.Designer.cs
@@ -33,7 +33,11 @@ namespace InfinityFiction.UI.InfinityFictionEditor
             this.components = new System.ComponentModel.Container();
             this.dataContext = new System.Windows.Forms.BindingSource(this.components);
             this.treeResources = new InfinityFiction.UI.InfinityFictionEditor.Core.WinFormControls.DataTreeView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.dataContext)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataContext
@@ -48,7 +52,7 @@ namespace InfinityFiction.UI.InfinityFictionEditor
             this.treeResources.HideSelection = false;
             this.treeResources.HotTracking = true;
             this.treeResources.IDColumn = "Id";
-            this.treeResources.Location = new System.Drawing.Point(12, 12);
+            this.treeResources.Location = new System.Drawing.Point(12, 85);
             this.treeResources.Name = "treeResources";
             this.treeResources.NameColumn = "Name";
             this.treeResources.ParentIDColumn = "ParentId";
@@ -56,16 +60,44 @@ namespace InfinityFiction.UI.InfinityFictionEditor
             this.treeResources.TabIndex = 0;
             this.treeResources.ValueColumn = "Id";
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(737, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(737, 597);
             this.Controls.Add(this.treeResources);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainForm";
             this.Text = "MainForm";
             ((System.ComponentModel.ISupportInitialize)(this.dataContext)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -73,5 +105,8 @@ namespace InfinityFiction.UI.InfinityFictionEditor
 
         private System.Windows.Forms.BindingSource dataContext;
         private DataTreeView treeResources;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
     }
 }
